1d41b08f76eb91d353cae7ecb1579e6951851686
Workout OS <workoutos@example.com>
2026-07-28 23:05:08 +0530

    Remove no-op fetch handler from sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,3 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-  // Let the browser handle the requests, this is just a dummy fetch listener
-  // required by some older browsers to trigger the PWA install prompt.
-});
